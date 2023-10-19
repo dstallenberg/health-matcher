05da3c64850ea3e00b62d0af6cee378d16d82c22
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2023-10-19 12:28:45 +0200

    feat: finalize list and get additional info on click

--- services/frontend/src/components/CareRequestList.vue
+++ services/frontend/src/components/CareRequestList.vue
@@ -9,7 +9,7 @@
   
       <v-card-text>
         <v-list>
-            <v-list-item v-for="(value, index) in requests" :key="index">
+            <!-- <v-list-item v-for="(value, index) in requests" :key="index">
                 <v-list-item-title>
                  {{ value.kind }} {{ value.name }}
                 </v-list-item-title>
@@ -19,7 +19,40 @@
                 <v-divider inset/>
                 {{ value.additionalInformation }}
                 <v-divider inset/>
-            </v-list-item>
+            </v-list-item> -->
+
+            <v-dialog width="500" v-for="(value, index) in requests" :key="index">
+              <template v-slot:activator="{ props }">
+                <v-list-item v-bind="props" text="Open Dialog"> 
+                  {{ value.name }}
+                  <v-list-item-subtitle>
+                    {{ (new Date(value.start)).toLocaleString() }} -> {{ (new Date(value.end)).toLocaleString() }}
+                  </v-list-item-subtitle>
+                </v-list-item>
+              </template>
+
+              <template v-slot:default="{ isActive }">
+                <v-card title="Care Request Information">
+                  <v-card-text>
+                    <b>name:</b> {{ value.name }}<br>
+                    <b>type:</b> {{  value.kind }}<br>
+                    <b>Start:</b> {{ (new Date(value.start)).toLocaleString() }}<br>
+                    <b>End:</b> {{ (new Date(value.end)).toLocaleString() }}<br>
+                    <b>Additional Information:</b><br>
+                    {{ value.additionalInformation }}
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      text="Apply"
+                      @click="isActive.value = false && apply(value.id)"
+                    ></v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
         </v-list>
       </v-card-text>
     </v-card>
@@ -40,6 +73,9 @@ onMounted(() => {
     store.fetchCareRequests()
 })
 
+function apply(id: string) {
+  // TODO
+}
 
   </script>
   
